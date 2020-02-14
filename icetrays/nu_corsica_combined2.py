f757b69d23dfda8f9ab2323ee6add47ced91324a
Pablo <pablo.r.c@live.com>
2020-02-14 14:49:44 +0100

    Na

--- icetrays/nu_corsica_combined2.py
+++ icetrays/nu_corsica_combined2.py
@@ -251,7 +251,7 @@ def run(i3_file):
     tray.AddModule(save_to_array, 'save',Streams=[icetray.I3Frame.Physics])
 
     print("Saving")
-    save_to_array('track_length')
+    #save_to_array('track_length')
 
     tray.Execute()
     tray.Finish()
