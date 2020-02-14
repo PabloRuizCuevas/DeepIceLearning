f964f9adb3907f5ea59f1c14c2e7bc07330f7f49
Pablo <pablo.r.c@live.com>
2020-02-14 14:36:26 +0100

    Na

--- icetrays/nu_corsica_combined2.py
+++ icetrays/nu_corsica_combined2.py
@@ -183,7 +183,66 @@ list0=np.array(os.listdir(sys.argv[1]))
 
 
 def run(i3_file):
-    # I3Tray Defintion
+    """IceTray script that wraps around an i3file and fills the events dict
+       that is initialized outside the function
+
+    Args:
+        i3_file, and IceCube I3File
+    Returns:
+        True (IceTray standard)
+    """
+
+    # Initialize
+    events = dict()
+    events['reco_vals'] = []
+    events['pulses'] = []
+    events['waveforms'] = []
+    events['pulses_timeseries'] = []
+    events['t0'] = []
+    surface = icecube.MuonGun.ExtrudedPolygon.from_file(geo_file, padding=0)
+    def save_to_array(phy_frame):
+        """Save the waveforms pulses and reco vals to lists.
+
+        Args:
+            phy_frame, and I3 Physics Frame
+        Returns:
+            True (IceTray standard)
+        """
+        reco_arr = []
+        pulses = None
+        for el in settings:
+            if el[1] == pulsemap_key:
+                try:
+                    pulses = phy_frame[pulsemap_key].apply(phy_frame)
+                except Exception as inst:
+                    print('Failed to add pulses {} \n {}'.format(el[1], inst))
+                    print inst
+                    return False
+            elif el[0] == 'variable':
+                try:
+                    reco_arr.append(eval('phy_frame{}'.format(el[1])))
+                except Exception as inst:
+#                    print inst
+                    reco_arr.append(np.nan)
+            elif el[0] == 'function':
+                try:
+                    reco_arr.append(
+                        eval(el[1].replace('_icframe_', 'phy_frame, geometry_file')))
+                except Exception as inst:
+                    print('Failed to evaluate function {} \n {}'.format(el[1], inst))
+                    return False
+        if pulses is not None:
+            tstr = 'Append Values for run_id {}, event_id {}'
+            eheader = phy_frame['I3EventHeader']
+            print(tstr.format(eheader.run_id, eheader.event_id))
+            events['t0'].append(get_t0(phy_frame))
+            events['pulses'].append(pulses)
+            events['reco_vals'].append(reco_arr)
+        else:
+            print('No pulses in Frame...Skip')
+            return False
+        return
+
     tray = I3Tray()
     tray.AddModule("I3Reader","source", FilenameList=[i3_file])
     tray.AddModule(classify_wrapper, "classify",surface=surface,Streams=[icetray.I3Frame.Physics])
