0783278eb47ca57045f9848d5af2a0cd55bbf779
Pablo <pablo.r.c@live.com>
2020-02-14 16:26:09 +0100

    Na

--- icetrays/nu_corsica_combined2.py
+++ icetrays/nu_corsica_combined2.py
@@ -83,7 +83,7 @@ def run(i3_file):
     tray.AddModule("I3Reader","source", FilenameList=i3_file)
     tray.AddModule( reco_q.classify_wrapper, "classify",surface=None,Streams=[icetray.I3Frame.Physics])
     tray.AddModule( reco_q.track_length_in_detector, 'track_length', surface=None,Streams=[icetray.I3Frame.Physics])
-    tray.AddModule( reco_q.save_to_array, 'save',Streams=[icetray.I3Frame.Physics])
+    tray.AddModule( save_to_array, 'save',Streams=[icetray.I3Frame.Physics])
 
     print("Saving")
     #save_to_array('track_length')
